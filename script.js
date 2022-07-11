83d82798a0688f3237249c3c1aa9d33ad74ee4c5
Rafael Accetta <rafaelaccetta@gmail.com>
2022-07-11 17:46:55 -0300

    Add managing wins and draws

--- script.js
+++ script.js
@@ -20,17 +20,24 @@ const gameboard = (() => {
 const displayController = (() => {
     const displayCells = document.querySelectorAll(".cell");
     const restartButton = document.getElementById("restart-button");
+    let _gameOver = false;
+    const setEndText = (text) => {
+        document.getElementById('end-banner').textContent = text;
+        document.getElementById('end-banner').classList.add('active');
+        _gameOver = true
+
+    }
 
     displayCells.forEach((cell) => {
         cell.addEventListener("click", (e) => {
-            /*if (gameController.getIsOver() || e.target.classList.contains('X') || e.target.classList.contains('O')) return */
-            gameController.handleClick(parseInt(e.target.dataset.index));
-            updateDisplay();
+            if (!_gameOver){
+                gameController.handleClick(parseInt(e.target.dataset.index));
+                updateDisplay();
+            };
         })
     });
 
     restartButton.addEventListener("click", (e) => {
-        console.log('restart');
         gameboard.reset();
         gameController.resetCurrentSymbol();
         resetDisplay()
@@ -50,11 +57,25 @@ const displayController = (() => {
         for(let i = 0; i < displayCells.length; i++) {
             displayCells[i].classList.remove('X', 'O')
         }
+        setEndText('');
+        document.getElementById('end-banner').classList.remove('active');
+        _gameOver = false;
     }
+    return {setEndText}
 })()
 
 const gameController = (() => {
     let _currentSymbol = 'X';
+    const _winCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 
     const switchCurrentSymbol = () => {
         _currentSymbol = (_currentSymbol == 'X') ? 'O' : 'X'
@@ -68,22 +89,34 @@ const gameController = (() => {
         return _currentSymbol
     };
 
+    const checkWin = () => {
+        return _winCombinations.some(combination => {
+            return combination.every(index => (gameboard.getCell(index) === _currentSymbol))
+        })
+    };
+
+    const checkDraw = () => {
+        for (let i = 0; i < 9; i++) {
+            if (gameboard.getCell(i) == '') return false
+        };
+        return true
+    }
+
+    const handleWin = () => displayController.setEndText(`${_currentSymbol} wins!`);
+
+    const handleDraw = () => displayController.setEndText(`It's a draw!`)
+
     const handleClick = (index) => {
         if (gameboard.getCell(index) === ''){
             gameboard.setCell(index, _currentSymbol)
-            for (let i = 0; i < 9; i++) {console.log(gameboard.getCell(i))};
-            console.log('--------------------')
+            if (checkWin()) handleWin()
+            else if (checkDraw()) handleDraw()
             switchCurrentSymbol()
 
         }
     };
 
-    const reset = () => {
-        gameboard.reset()
-        _currentSymbol = 'X'
-    };
-
-    return {handleClick, reset, switchCurrentSymbol, resetCurrentSymbol}
+    return {handleClick, resetCurrentSymbol}
 })()
 
 
